5c703ebae7fd439e790ae3d02833f98b0d62f6fb
xm94 <xavier_martinez94@yahoo.com>
2020-11-06 17:50:56 -0700

    removing token

---
+++ apis/twitter_search_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+
+endpoint="https://api.twitter.com/2/tweets/search/recent?query=covid-19&tweet.fields=created_at,author_id,lang,context_annotations&max_results=100"
+headers = {"Authorization": "Bearer Token"}
+
+res = requests.get(endpoint,headers=headers)
+
+twitter = json.loads(res.content)
+tweet=twitter['data'][0]
+
+fields=tweet.keys()
+print(fields)
+print()
+
+for key in fields:
+    print(key,":",tweet[key])
+    print()
