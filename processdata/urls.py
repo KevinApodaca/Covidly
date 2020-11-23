df127a62d4ef562e8a26ef41e65fbdef38b64221
xm94 <xavier_martinez94@yahoo.com>
2020-11-23 03:22:58 -0700

    created initial state of newsfeed page for twitter

--- processdata/urls.py
+++ processdata/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('cases', views.global_cases, name='cases'),
     path('realtime_growth', views.realtime_growth, name='realtime_growth'),
     path('daily_growth', views.daily_growth, name='daily_growth'),
-    path('daily_report', views.daily_report, name='daily_report')
+    path('daily_report', views.daily_report, name='daily_report'),
+    path('tweets',views.tweets, name='tweets')
 ]
