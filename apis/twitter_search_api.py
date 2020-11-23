8776e20d4b17b19ad2c46a8dfb058a5ff51e9aeb
xm94 <xavier_martinez94@yahoo.com>
2020-11-23 04:04:59 -0700

    unused code

--- apis/twitter_search_api.py
+++ apis/twitter_search_api.py
@@ -12,7 +12,7 @@ arg_names={'fields':"&tweet.fields="
 default_fields=['created_at','author_id','lang','possibly_sensitive']
 default_expansions=['referenced_tweets.id','author_id']#author_id necessary here as well to retrieve author info
 default_user=['name','username']
-token= ''
+token= 'AAAAAAAAAAAAAAAAAAAAAMbOIQEAAAAAmOubtSTp%2Fdme345uVc8IOIys4%2Fk%3DcG2CuEuQ5xQppMgn13akWLyvxwxMOiDXIyF25glaQLcnRhNXSu'
 results='100'
 headers = {"Authorization": "Bearer " + token}
 base_endpoint="https://api.twitter.com/2/tweets/search/recent?query="
@@ -97,9 +97,8 @@ def fetch_tweets(query=default_query,fields=default_fields,expansions=default_ex
         oe_endpoint = oembed_endpoint+t['author_username']+'/status/'+t['id']+oe_mode
         res = requests.get(oe_endpoint,headers=headers)
         embed = json.loads(res.content)
-        t_fields = t.keys()
         for k in t.copy().keys():
-            if(k!='created_at'):
+            if(k!='created_at' and k!='text'):
                 t.pop(k)
         t['oembed']=embed['html']
         
