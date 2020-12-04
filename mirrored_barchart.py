398be8f163200be9a7d24afc565cb6089dfb50a7
Jacob Barberena <jbarberena@miners.utep.edu>
2020-12-04 15:26:08 -0700

    Implemented API calls to get texas county data

--- mirrored_barchart.py
+++ mirrored_barchart.py
@@ -1,3 +1,4 @@
+from apis import covid_daily_api
 import plotly
 import plotly.graph_objects as go
 
@@ -6,24 +7,31 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 
-#   IMPORTANT NOTE: 
-#     I used Sourcerree to merge 'Trelo-12-covidAPI' into 'Trello-24-mirrored_barchart' to get the updated covi_daily_api.py file 
-#     before having the full merge pull request was approved, not sure if this would affect stuff
+#   NOTES: 
+#     - Used Sourcerree to merge 'Trelo-12-covidAPI' into 'Trello-24-mirrored_barchart' to get the updated covi_daily_api.py file 
+#     - needs further testing
 
+texas_counties = ['Harris', 'Dallas', 'Tarrant', 'Bexar', 'Travis', 'Collin', 'Denton', 'Hidalgo', 'El Paso', 'Fort Bend']
+
+
+allCountiesCases = []
+for county in texas_counties:
+    county_cases = covid_daily_api.casesCounty(county, "Texas")
+    allCountiesCases.append(county_cases)
+print(allCountiesCases)
+
+allCountiesDeaths = []
+for county in texas_counties:
+    county_death = covid_daily_api.deathsCounty(county, "Texas")
+    allCountiesDeaths.append(county_death)
+print(allCountiesDeaths)
 
 citiesArr = ['Houston','San Antonio','Dallas', 'Austin', 'Fort Worth', 
     'El Paso', 'Arlington', 'Corpus Cristi', 'Plano', 'Laredo']
 
-#       For Sebastian's Flight Data: 
-#arlington and plano not supported
-# Lubbock and amarillo are good
-
-
-#use Valeria's NY cities data
-
     # -- implement API calls to avoid hard code data
-cases = [3270, 2400, 1200, 1400, 2420, 3000, 900, 700, 800, 1000]
-deaths = [320, 230,  455,  830, 350, 350, 300, 215, 610, 665]
+cases = [3270, 2400, 1200, 1400, 2420, 3000, 900, 700, 800, 1000] #can set cases=allCountiesCases rather than sample data or change in trace x axis
+deaths = [320, 230,  455,  830, 350, 350, 300, 215, 610, 665]     #can set deaths=allCountiesDeaths rather than sample data or change in trace x axis and neg_deaths
 
     # -- loop ready for API call implemetation (assuming data returns a single number), just need to set: cases, deaths = []
 #for city in citiesArr:
@@ -37,7 +45,7 @@ for i in range(len(deaths)):
 
 fig = go.Figure()
 
-fig.add_trace(go.Bar(x=cases[::-1], y=citiesArr[::-1],
+fig.add_trace(go.Bar(x=cases[::-1], y=citiesArr[::-1], #replace cittiesArr with texas_counties
                 base=0,
                 marker_color='orange',
                 name='Cases',
@@ -47,7 +55,7 @@ fig.add_trace(go.Bar(x=cases[::-1], y=citiesArr[::-1],
                 insidetextanchor = 'start',
                 hoverinfo = 'y',                #change to 'none' to disable bar hover text
                 ) )
-fig.add_trace(go.Bar(x=neg_deaths[::-1], y=citiesArr[::-1],
+fig.add_trace(go.Bar(x=neg_deaths[::-1], y=citiesArr[::-1],  #replace cittiesArr with texas_counties
                 base=0,
                 marker_color='crimson',
                 name='Deaths',
@@ -60,12 +68,12 @@ fig.add_trace(go.Bar(x=neg_deaths[::-1], y=citiesArr[::-1],
 
 
 fig.update_layout(
-    title = "Cases vs Deaths per Major City in Texas",
+    title = "Cases vs Deaths per Major County in Texas",
     autosize=False,
     width=800,          # this is the size the graph will actually take up,
     barmode = 'stack',   #   can be removed to show so plotly fits automaticaly
 )
-#fig.show()
+fig.show()
 
 #'''
 #region
