9b4735d1d577323223ccd0e3e32fef84dc168127
Gilbert <gvelasquez2@miners.utep.edu>
2020-12-05 18:34:26 -0700

    Moved methods from TopStates.py into processdata and added a methog in views.py for the request

--- processdata/getdata.py
+++ processdata/getdata.py
@@ -9,6 +9,7 @@ import datetime
 import requests
 import platform
 import pandas as pd
+from apis import covid_daily_api
 
 # Global access key for flights API
 access_key = '11e7b1fd08232c292d1fe70992fc2a1d'
@@ -214,3 +215,93 @@ def news_articles():
     articles = articles[:6]
     df = pd.DataFrame(articles, columns=['title', 'description', 'url', 'urlToImage'])
     return df
+
+def top_states():
+    TXCases, TXDates = covid_daily_api.getMonthlyCases("texas")
+    CACases, CADates = covid_daily_api.getMonthlyCases("california")
+    NYCases, NYDates = covid_daily_api.getMonthlyCases("new york")
+    UTCases, UTDates = covid_daily_api.getMonthlyCases("utah")
+    FLCases, FLDates = covid_daily_api.getMonthlyCases("florida")
+    return createFigure(TXCases,TXDates,CACases,CADates,NYCases,NYDates,UTCases,UTDates,FLCases,FLDates)
+
+def fixAxis(TXCases,TXDates,CACases,CADates,NYCases,NYDates,UTCases,UTDates,FLCases,FLDates):
+    for i in range(len(TXDates)):
+        monthStr =str(TXDates[i])[:2]
+        dayStr = str(TXDates[i] % 100)
+        result = (monthStr + "/" + dayStr + "/20")
+        TXDates[i] = result
+
+        monthStr =str(CADates[i])[:2]
+        dayStr = str(CADates[i] % 100)
+        result = (monthStr + "/" + dayStr + "/20")
+        CADates[i] = result
+
+        monthStr =str(NYDates[i])[:2]
+        dayStr = str(NYDates[i] % 100)
+        result = (monthStr + "/" + dayStr + "/20")
+        NYDates[i] = result
+
+        monthStr =str(UTDates[i])[:2]
+        dayStr = str(UTDates[i] % 100)
+        result = (monthStr + "/" + dayStr + "/20")
+        UTDates[i] = result
+
+        monthStr =str(FLDates[i])[:2]
+        dayStr = str(FLDates[i] % 100)
+        result = (monthStr + "/" + dayStr + "/20")
+        FLDates[i] = result
+
+def createFigure(TXCases,TXDates,CACases,CADates,NYCases,NYDates,UTCases,UTDates,FLCases,FLDates):
+    day = (int(datetime.today().strftime("%d")) - 1)
+    fixAxis(TXCases,TXDates,CACases,CADates,NYCases,NYDates,UTCases,UTDates,FLCases,FLDates)
+
+    StateGraph = go.Figure()
+    StateGraph.add_trace(go.Scatter(x = TXDates[::-1], y = TXCases[::-1], mode = "lines+markers", name = "Texas", visible= True, line= dict(color = "red"))) # Texas
+    StateGraph.add_trace(go.Scatter(x = CADates[::-1], y = CACases[::-1], mode = "lines+markers", name = "California", visible = False, line= dict(color = "blue"))) # California
+    StateGraph.add_trace(go.Scatter(x = NYDates[::-1], y = NYCases[::-1], mode = "lines+markers", name = "New York", visible = False, line= dict(color = "orange"))) # New York
+    StateGraph.add_trace(go.Scatter(x = UTDates[::-1], y = UTCases[::-1], mode = "lines+markers", name = "Utah", visible = False, line= dict(color = "purple"))) # Utah
+    StateGraph.add_trace(go.Scatter(x = FLDates[::-1], y = FLCases[::-1], mode = "lines+markers", name = "Florida", visible = False, line= dict(color = "green"))) # Florida
+
+    StateGraph.update_layout(
+        updatemenus=[
+            dict(
+                type="buttons",
+                direction="right",
+                active=0,
+                x=0.57,
+                y=1.2,
+                buttons=list([
+                    dict(label="Texas",
+                         method="update",
+                         args=[{"visible": [True,False,False,False,False]},
+                               {"title": "Texas Active Cases",
+                                "annotations": []}]),
+                    dict(label="California",
+                         method="update",
+                         args=[{"visible": [False,True,False,False,False]},
+                               {"title": "California Active Cases",
+                                "annotations": []}]),
+                    dict(label="New York",
+                         method="update",
+                         args=[{"visible":[False,False,True,False,False]},
+                               {"title": "New York Active Cases",
+                                "annotations": []}]),
+                    dict(label="Utah",
+                         method="update",
+                         args=[{"visible": [False,False,False,True,False] },
+                               {"title": "Utah Active Cases",
+                                "annotations": []}]),
+                    dict(label="Florida",
+                         method="update",
+                         args=[{"visible":[False,False,False,False,True] },
+                               {"title": "Florida Active Cases",
+                                "annotations": []}]),
+                    dict(label="All",
+                         method="update",
+                         args=[{"visible":[True,True,True,True,True]},
+                               {"title": "States Active Cases",
+                                "annotations": []}]),
+                ]),
+            )
+        ])
+    return StateGraph
